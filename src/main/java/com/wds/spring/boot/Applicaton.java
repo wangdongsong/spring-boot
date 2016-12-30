ef154649ab2a5a41cebb014b88117b33db495e1d
wangdongsong <wangdongsong846@pingan.com.cn>
2016-12-30 16:36:09 +0800

    SpringBoot启动主类

---
+++ src/main/java/com/wds/spring/boot/Applicaton.java
@@ -0,0 +1,18 @@
+package com.wds.spring.boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by WANGDONGSONG846 on 2016-12-29.
+ */
+@SpringBootApplication
+public class Applicaton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Applicaton.class);
+    }
+
+
+}
